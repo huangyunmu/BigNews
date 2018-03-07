75b47dec76887ec02faf302883552a9c084d35db
huangyunmu <andyhuangyunmu@gmail.com>
2018-03-07 11:52:44 +0800

    Add LexicalAnalysis API

--- API/APISet.py
+++ API/APISet.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 
 @author: andyh
@@ -100,6 +101,54 @@ class ContentGrab(object):
             return None
         else:
             return data
+class LexicalAnalysis(object):
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    @staticmethod
+    def getLexicalAnalysis(text, code=0x00200000, searchType=0):
+        '''
+    parameter:
+        text:The text that need to be split
+        code:The coding scheme 
+        searchType:The search type 
+        取值0或1，默认为0。 0为基础粒度版分词，倾向于将句子切分的更细，在搜索场景使用为佳。 1为混合粒度版分词，倾向于保留更多基本短语不被切分开。
+    return:
+        combtokens:The phrase obtained 
+        tokens:The token obtained
+    '''
+        module = 'wenzhi'
+        action = 'LexicalAnalysis'
+        region = "sz"
+        path = DataLoader.getDataPath("keys/keys.txt")
+        with open(path) as f:
+            secretId = f.readline().strip('\n')
+            secretKey = f.readline().strip('\n')
+        config = {
+            'Region': region,
+            'secretId': secretId,
+            'secretKey': secretKey}
+        action_params = {
+            "text":text,
+            "code":code,
+            "type":searchType
+        }
+        try:
+            service = QcloudApi(module, config)
+            print(service.generateUrl(action, action_params))
+            result = service.call(action, action_params)
+            print(result)
+        except Exception as e:
+            print('traceback.format_exc():\n%s' % traceback.format_exc())
+        if(result == None):
+            return None, None
+        data = json.loads(result.decode('utf-8'))
+        if(result != None and data["code"] != 0):
+            return None, None
+        else:
+            return data["combtokens"], data["tokens"]
 class ContentTranscode(object):
     def __init__(self):
         '''
--- API/APITest.py
+++ API/APITest.py
@@ -4,9 +4,7 @@
 @author: andyh
 '''
 import sys
-import json
 from API import APISet
-from Util import DataLoader
 
 
 sys.path.append("..")
@@ -14,30 +12,61 @@ if __name__ == '__main__':
 #     optional 
 #   KeyWord Test
 #     channel = "CHnews_news_tech"
-#     title = "Dior新款，秋冬新款娃娃款甜美圆领配毛领毛呢大衣外套、码数：SM、P330"
-#     content = "Dior新款，秋冬新款娃娃款甜美圆领配毛领毛呢大衣外套、码数：SM、P330"
-#     data = APISet.TextKeywords.getKeyword(title, content)
-#     if(data != None):
-#         for item in data:
-#             print("keyword=", item['keyword'])
-#             print("score=", item['score'])
-#             print("type=", item['type'])
-#     else:
-#         print("No data return")
-
-# contentGrabTest
-    url = 'http://new.qq.com/cmsn/20180305/20180305005930'
-    data = APISet.ContentGrab.getContent(url)
+    title = "闽宁镇的故事"
+    content = """
+    闽宁镇，这个当年由习近平亲自命名的扶贫移民区，已从一个“天上无飞雀，地上不长草，风吹沙石跑”的荒芜之地，发展成为常住居民6万余人的特色小镇。
+　　1997年，福建和宁夏启动了对口扶贫协作。作为起点，闽宁镇从无到有、由弱到强，见证了闽、宁携手从单向扶贫到互利共赢的20载
+　　2016年7月19日，习总书记到闽宁镇视察时曾给予高度肯定并指出：“闽宁镇从当年的‘干沙滩’变成了今天的‘金沙滩’，探索出了一条康庄大道，我们要把这个宝贵经验向全国推广。”
+　　如今，曾经的戈壁荒滩变成了现代化的生态移民示范镇，6万多名曾经生活在贫困山区的农民走出大山搬入闽宁镇，通过移民搬迁走上了脱贫致富之路。
+    """
+    data = APISet.TextKeywords.getKeyword(title, content)
     if(data != None):
-        print("code=",data['code'])
-        print("title=",data['title'])
-        print("content=",data['content'])
+        for item in data:
+            print("keyword=", item['keyword'])
+            print("score=", item['score'])
+            print("type=", item['type'])
     else:
         print("No data return")
-    
+
+# contentGrabTest
+#     url = 'http://new.qq.com/cmsn/20180305/20180305005930'
+#     data = APISet.ContentGrab.getContent(url)
+#     if(data != None):
+#         print("code=", data['code'])
+#         print("title=", data['title'])
+#         print("content=", data['content'])
+#     else:
+#         print("No data return")
+        
+        
+#     ContentTranscode Test
 #     url='http://gd.ifeng.com/a/20180305/6408870_0.shtml'
 #     data = APISet.ContentTranscode.getContentTranscode(url)
 #     print(data)
+
+    text = """
+    闽宁镇，这个当年由习近平亲自命名的扶贫移民区，已从一个“天上无飞雀，地上不长草，风吹沙石跑”的荒芜之地，发展成为常住居民6万余人的特色小镇。
+　　1997年，福建和宁夏启动了对口扶贫协作。作为起点，闽宁镇从无到有、由弱到强，见证了闽、宁携手从单向扶贫到互利共赢的20载
+　　2016年7月19日，习总书记到闽宁镇视察时曾给予高度肯定并指出：“闽宁镇从当年的‘干沙滩’变成了今天的‘金沙滩’，探索出了一条康庄大道，我们要把这个宝贵经验向全国推广。”
+　　如今，曾经的戈壁荒滩变成了现代化的生态移民示范镇，6万多名曾经生活在贫困山区的农民走出大山搬入闽宁镇，通过移民搬迁走上了脱贫致富之路。
+    """
+    combtokens,tokens = APISet.LexicalAnalysis.getLexicalAnalysis(text)
+    if(combtokens != None):
+        for item in combtokens:
+#             print("cls=", item['cls'])
+#             print("pos=", item['pos'])
+#             print("wlen=", item['wlen'])
+            print("word=", item['word'])
+    if(tokens!=None):
+        for item in tokens:
+#             print("pos=", item['pos'])
+#             print("wlen=", item['wlen'])
+            print("word=", item['word'])
+            print("wtype=", item['wtype'])
+#             print("wtype_pos=",item["wtype_pos"])
+    
+    else:
+        print("No data return")
     
     
     
